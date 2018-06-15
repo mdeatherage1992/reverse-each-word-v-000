f7c8cd2cf1709848f1bcf10f6a9954c240072370
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 15:49:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
-  new_string = ''
+  new_string = []
   words = string.split(' ')
   words.collect do |word|
-  new_string += (word.reverse + " ")
+  new_string.push(word.reverse)
 end
-return new_string
+return new_string.join(' ')
 end
